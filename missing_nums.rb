8fd143d001fa2f961b6d03009827d0e9581ef053
John Gramila <tynanjk@gmail.com>
2016-04-13 11:04:24 -0500

    Edited array of missing numbers

--- missing_nums.rb
+++ missing_nums.rb
@@ -3,24 +3,41 @@ def missing_nums(array)
   number = 1
   index = 0
   missing_nums = []
-  for i in array
-    if index != 0 && i == array[index - 1]
+  #edit this number to change the size of array we are checking against
+  while number <= 10000
+    #index != 0 prevents the first and last number of the array being the same producing a false "true"
+    #checks duplicate numbers in the sorted array, if a number is duplicated, move to next element 
+    #returns false if the index position is greater than the length of the array
+    if index != 0 && array[index] == array[index - 1]  && index < array.length
       index += 1
-      elsif i != number
-      	missing_nums.push(number)
-      	number += 1
-      else i == number
+      #if the element of the array isn't equal to number, add the number to missing_nums and check the next number at the same index
+    elsif array[index] != number
+        missing_nums.push(number)
+        number += 1
+      #if number is in the array, check the next number at the next index
+      else array[index] == number
         number += 1
         index += 1
     end
   end
+  missing_nums
+end
+
+def slower_missing_nums(array)
+  number = 1
+  missing_nums = []
   while number <= 10000
-    missing_nums.push(number)
-    number += 1
+    if array.include?(number)
+     number += 1
+    else
+      missing_nums.push(number)
+      number += 1
+    end
   end
   missing_nums
 end
 
-array = [3,1,1,2,4,5,1]
+array = Array.new(9998) {|num| num += 1}
 puts missing_nums(array)
+puts better_missing_nums(array)
   
