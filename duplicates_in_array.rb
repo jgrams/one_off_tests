cc24f58d323b09878419abf820d5ae793ed990b8
John Gramila <tynanjk@gmail.com>
2016-10-05 00:26:34 -0500

    solution that doens't take up memory

--- duplicates_in_array.rb
+++ duplicates_in_array.rb
@@ -1,6 +1,15 @@
+#return all the duplicate numbers in an array
 array = [1,2,3,3]
 
-def return_dups_from_array(array)
-  for i in array
-    
+#solution that doesn't take up space in memory
+def return_duplicates_in_array(array)
+  array.sort.each_with_index  do |item, index|
+    #removes the numbers that are the same as proceedng numbers
+    if item == array[index-1]
+      array -= array[index]
+      #keeps numbers that are the same as the subsequent number
+      elsif item == array[index+1]
+      #removes numbers that are not the same as subsequent numbers
+      else array -= array[index]
+  end
 end 
